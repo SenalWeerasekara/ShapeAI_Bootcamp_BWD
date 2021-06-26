cd111cddc2d56b7c9ed2a409c59e76c028782742
SenalWeerasekara <86529094+SenalWeerasekara@users.noreply.github.com>
2021-06-26 22:13:35 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class = "note">
+      <h1> javascript and react.js</h1>
+      <p>a basic we dev react js bootcamp</p>
+    </div>
+  );
+}
+export default Info;
